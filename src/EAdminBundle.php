ab941ab340806365d586ee1399e3910822d3a8af
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2026-08-07 09:39:24 +0200

    Update paths

--- src/EAdminBundle.php
+++ src/EAdminBundle.php
@@ -14,7 +14,7 @@ class EAdminBundle extends AbstractBundle {
             $container->prependExtensionConfig("framework", [
                 "asset_mapper" => [
                     "paths" => [
-                        __DIR__ . "/EAdmin" => "eadmin",
+                        "%kernel.project_dir%/EAdmin" => "eadmin",
                     ]
                 ]
             ]);
@@ -23,7 +23,7 @@ class EAdminBundle extends AbstractBundle {
         if ($container->hasExtension("twig")) {
             $container->prependExtensionConfig('twig', [
                 'paths' => [
-                    __DIR__ . '/EAdmin' => 'EAdmin',
+                    '%kernel.project_dir%/EAdmin' => 'EAdmin',
                 ],
             ]);
         }
