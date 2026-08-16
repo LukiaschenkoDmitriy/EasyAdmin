bbb476d7c387899eb9a94d3e0e3f70bcc7520a8b
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2026-08-16 19:18:16 +0200

    remove var

--- var/cache/test/EAdmin_Core_Tests_Fixture_TestKernelTestDebugContainer.bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'ServicesBundle' => new \Symfony\Component\DependencyInjection\Kernel\ServicesBundle(),
-    'ConsoleBundle' => new \Symfony\Component\Console\ConsoleBundle(),
-    'FrameworkBundle' => new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-    'EAdminBundle' => new \EAdmin\Core\EAdminBundle(),
-];
