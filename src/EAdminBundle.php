048b48ab59f98803bb18390e3de52c2833433865
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2026-08-17 23:50:10 +0200

    configure bundle

--- src/EAdminBundle.php
+++ src/EAdminBundle.php
@@ -5,6 +5,8 @@ namespace EAdmin\Core;
 use EAdmin\Core\Assets\AssetMapperResolver;
 use EAdmin\Core\Assets\AssetResolverInterface;
 use EAdmin\Core\Assets\ViteAssetResolver;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Override;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -43,5 +45,33 @@ class EAdminBundle extends AbstractBundle {
             AssetResolverInterface::class,
             $config["assets"]["driver"] === "vite" ? ViteAssetResolver::class : AssetMapperResolver::class
         );
+    }
+
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+                ->arrayNode('assets')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->enumNode('driver')
+                            ->values(['vite', 'asset_mapper'])
+                            ->defaultValue('asset_mapper')
+                        ->end()
+                        ->arrayNode('vite')
+                            ->addDefaultsIfNotSet()
+                            ->children()
+                                ->booleanNode('typescript')->defaultTrue()->end()
+                                ->booleanNode('scss')->defaultTrue()->end()
+                                ->booleanNode('tailwind')->defaultFalse()->end()
+                                ->scalarNode('manifest_path')
+                                    ->defaultValue('%kernel.project_dir%/public/build/.vite/manifest.json')
+                                ->end()
+                                ->scalarNode('build_public_path')->defaultValue('/build/')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
     }
 }
