73627b0822a027da090f1548ccedef7ff8301b31
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2026-09-05 07:35:29 +0200

    Added services to page controller

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -11,8 +11,8 @@ use Symfony\Component\HttpFoundation\Response;
 abstract class PageController extends AbstractController {
     public function __construct(protected ComponentRenderer $renderer) {}
 
-    public function renderComponent(ComponentInterface $component, array $context = []): Response
+    public function renderComponent(ComponentInterface $component, array $context = [], array $services = []): Response
     {
-        return new Response(TidyUtil::cleanAndRepairHTML($this->renderer->render($component, $context)), 200);
+        return new Response(TidyUtil::cleanAndRepairHTML($this->renderer->render($component, $context, $services)), 200);
     }
 }
