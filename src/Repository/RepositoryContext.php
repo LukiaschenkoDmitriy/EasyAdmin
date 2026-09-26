bf21575017021d358e9392fd08253443763b5ca9
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2026-09-26 11:16:58 +0200

    Page number fix

--- src/Repository/RepositoryContext.php
+++ src/Repository/RepositoryContext.php
@@ -9,11 +9,11 @@ class RepositoryContext {
         public ?string $search = null,
         public ?string $searchBy = null,
         public int $limit = 25,
-        public int $page = 0,
+        public int $page = 1,
         public int $count = 0,
         public array $limits = [10,25,50,75,100]
     ) {
-        if ($this->page < 0) $this->page = 0;
+        if ($this->page < 1) $this->page = 1;
         if ($this->limit < min($this->limits)) $this->limit = min($this->limits);
         if ($this->limit > max($this->limits)) $this->limit = max($this->limits);
      }
